d7323102f99e2608715708e932879a12620dec24
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-07 16:22:55 +0300

    3.4

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
       
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+})
+
 app.get('/info', (req, res) => {
   res.send(info());
 })
