7473db2698fc3e4c060689133b50d973ec942790
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-07 16:04:51 +0300

    3.2

--- index.js
+++ index.js
@@ -22,17 +22,38 @@ let persons = [
         name: 'Lea Kutvonen',
         number: '040-123456',
     }
-    ]
+]
   
+
+info = () => {
+  let r = 'puhelinluettelossa '
+    .concat(  persons.length )
+    .concat( ' henkilön tiedot' )
+    .concat('<br>')
+    .concat(new Date())
+  return(r);
+} 
+
+help = () => {
+  let r = '/api/persons'
+  .concat('<br>')
+  .concat('/info');
+  return (r);
+}
+
 app.get('/', (req, res) => {
-        res.send('/api/persons')
-      })
+  res.send(help());
+})
       
 app.get('/api/persons', (req, res) => {
-        res.json(persons)
-      })
+  res.json(persons);
+})
       
+app.get('/info', (req, res) => {
+  res.send(info());
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
-      })
+  console.log(`Server running on port ${PORT}`)
+})
