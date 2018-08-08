83d98869dbf69ab57e4044c6d803ee0534e67ae1
pet787 <41019198+pet787@users.noreply.github.com>
2018-08-08 19:36:43 +0300

    3.13

--- index.js
+++ index.js
@@ -3,115 +3,113 @@ const app = express()
 const bodyParser = require('body-parser')
 var morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
+// MIDDLEWARE
 app.use(cors());
+app.use(bodyParser.json())
 app.use(express.static('build'));
-
 morgan.token('body', function getBody(req) { 
   return JSON.stringify( req.body ) 
 })
-
 app.use(
   morgan(':method :url :body :status :res[content-length] - :response-time ms')
 )
 
-app.use(bodyParser.json())
-
-let persons = [
-    {
-      id: 1,
-      name: 'Arto Hellas',
-      number: '040-123456',
-    },
-    {
-      id: 2,
-      name: 'Martti Tienari',
-      number: '040-123456',
-    },
-    {
-      id: 3,
-      name: 'Arto Järvinen',
-      number: '040-123456',
-    },
-    {
-        id: 4,
-        name: 'Lea Kutvonen',
-        number: '040-123456',
-    }
-]
-  
-info = () => {
-  let r = 'puhelinluettelossa '
-    .concat(  persons.length )
-    .concat( ' henkilön tiedot' )
-    .concat( '<br>' )
-    .concat(new Date())
-  return(r);
-} 
-
-help = () => {
-  let r = '/api/persons'
-  .concat('<br>')
-  .concat('/info');
-  return (r);
-}
-
-randomBetween = ( min, max ) => {
-  return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+const formatPerson = (person) => {
+  console.log('formatPerson', person)
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
 }
 
 app.get('/', (req, res) => {
-  res.send(help());
+  res.send('REST API: /api/persons');
 })
-      
+
+// GET ALL
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person
+  .find({})
+  .then(persons => {
+    res.json(persons.map(formatPerson))
+  })
 })
 
+// GET ONE
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    res.json(person);
-  }
-  else {
-    res.status(404).end();
-  }
+  Person
+  .findById(req.params.id)
+  .then(person => {
+    if (person) {
+      res.json(formatPerson(person))
+    } else {
+      res.status(404).end()
+    }
+  })
+  .catch(error => {
+    console.log(error)
+    res.status(400).send({ error: 'malformatted id' })
+  })
 })
 
+// POST
 app.post('/api/persons', (req, res) => {
-  const body = req.body;
-  const randomId = randomBetween(100000000000, 999999999999);
-  const newPerson = {
-    id : randomId,
+  const body = req.body
+  console.log(req)
+
+  const person = new Person({
     name: body.name,
     number: body.number
-  };
-  if (!newPerson.name) {
+  })
+
+  if (!person.name) {
     return res.status( 400 ).json( { error: 'Name missing' } ) ;
   };
-  if (!newPerson.number) {
+  if (!person.number) {
     return res.status( 400 ).json( { error: 'Number missing' } ); 
   };
-  exists = persons.find(person => person.name === newPerson.name);
+/*   exists = persons.find(person => person.name === newPerson.name);
   if (exists) {
     return res.status( 400 ).json( { error: 'Name must be unique' } ); 
   }
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
+ */
+  person
+  .save()
+  .then(formatPerson)
+  .then(savedAndFormattedPerson => {
+    res.json(savedAndFormattedPerson)
+  })
 })
       
+// DELETE
 app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id);
-  res.status(204).end();
+  Person
+  .findByIdAndRemove(req.params.id)
+  .then(result => {
+    res.status(204).end()
+  })
+  .catch(error => {
+    res.status(400).send({ error: 'malformatted id' })
+  })
 })
 
+// GET INFO
 app.get('/info', (req, res) => {
   res.send(info());
 })
 
-const PORT = 3001
+  // NOT HANDLED ROUTE
+  /* const error = (req, res) => {
+    res.status(404).send({error: 'unknown endpoint'})
+  }
+
+  app.use(error)
+ */
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
